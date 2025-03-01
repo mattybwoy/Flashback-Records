cf1755f755ed1a1f5a7fcbee58593dcd47d23627
Matthew Lock <matthew_lock20@hotmail.com>
2025-03-01 16:51:47 +0000

    Initial Commit

---
+++ Flashback-RecordsUITests/Flashback_RecordsUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Flashback_RecordsUITestsLaunchTests.swift
+//  Flashback-RecordsUITests
+//
+//  Created by Matthew Lock on 01/03/2025.
+//
+
+import XCTest
+
+final class Flashback_RecordsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
