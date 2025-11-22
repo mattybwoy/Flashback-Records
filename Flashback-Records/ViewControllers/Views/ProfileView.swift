813170aab808ddb13b460d8727e0dda5fd01dbb7
Matthew Lock <matthew_lock20@hotmail.com>
2025-11-22 15:17:48 +0000

    add buttons to profileView

--- Flashback-Records/ViewControllers/Views/ProfileView.swift
+++ Flashback-Records/ViewControllers/Views/ProfileView.swift
@@ -14,6 +14,8 @@ protocol ProfileScreenInterface {
 
 struct ProfileView: View, ProfileScreenInterface {
     
+    @State private var email: String = ""
+    
     var profileView: some View {
         self
     }
@@ -21,7 +23,67 @@ struct ProfileView: View, ProfileScreenInterface {
     var body: some View {
         Text("PROFILE")
             .font(.appTitle)
-        Color.purple
+
+        Spacer()
+        
+        VStack(spacing: 12) {
+            Button {
+                //Action to open Modal sign in view
+            } label: {
+                Text("Sign In")
+            }
+            .padding(.horizontal, 36)
+            .padding(.vertical, 16)
+            .overlay(Rectangle()
+                .stroke(Color.black, lineWidth: 4))
+            .padding(.vertical, 8)
+            
+            
+            Button {
+                //Action to open Modal sign up view
+            } label: {
+                Text("Sign Up")
+            }
+            .padding(.horizontal, 36)
+            .padding(.vertical, 14)
+            .overlay(Rectangle()
+                .stroke(Color.black, lineWidth: 4))
+            .padding(.vertical, 8)
+        }
+        
+        Spacer()
+        
+        VStack(spacing: 12) {
+            Text("Subscribe to our newsletter")
+            
+            Text("Be the first to know about new collections and receive email-only discounts.")
+            
+            TextField("Email", text: $email)
+            
+            Button("Subscribe") {
+                // Action to subscribe to newsletter
+            }
+            
+        }
+        .padding(24)
+        
+        VStack(spacing: 4) {
+            Link("Privacy Policy", destination: URL(string: "https://flashback.co.uk/policies/privacy-policy")!)
+                .font(.title)
+                .foregroundStyle(.red)
+            Link("Returns Policy", destination: URL(string: "https://flashback.co.uk/pages/returns-policy")!)
+                .font(.title)
+                .foregroundStyle(.red)
+            Link("Shipping Policy", destination: URL(string: "https://flashback.co.uk/pages/shipping-policy")!)
+                .font(.title)
+                .foregroundStyle(.red)
+            Link("Contact Us", destination: URL(string: "https://flashback.co.uk/pages/contact-us")!)
+                .font(.title)
+                .foregroundStyle(.red)
+            Link("About Us", destination: URL(string: "https://flashback.co.uk/pages/about-us")!)
+                .font(.title)
+                .foregroundStyle(.red)
+        }
     }
 }
 
