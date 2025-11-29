82ca38081070c2ec7ab39e3eaf08f256b65d3760
Matthew Lock <matthew_lock20@hotmail.com>
2025-11-29 12:02:46 +0000

    profileView UI updates

--- Flashback-Records/ViewControllers/Views/ProfileView.swift
+++ Flashback-Records/ViewControllers/Views/ProfileView.swift
@@ -23,14 +23,15 @@ struct ProfileView: View, ProfileScreenInterface {
     var body: some View {
         Text("PROFILE")
             .font(.appTitle)
-
-        Spacer()
+            .padding(.bottom, 24)
         
         VStack(spacing: 12) {
             Button {
                 //Action to open Modal sign in view
             } label: {
                 Text("Sign In")
+                    .font(.appBody)
+                    .foregroundStyle(.black)
             }
             .padding(.horizontal, 36)
             .padding(.vertical, 16)
@@ -43,6 +44,8 @@ struct ProfileView: View, ProfileScreenInterface {
                 //Action to open Modal sign up view
             } label: {
                 Text("Sign Up")
+                    .font(.appBody)
+                    .foregroundStyle(.black)
             }
             .padding(.horizontal, 36)
             .padding(.vertical, 14)
@@ -55,34 +58,50 @@ struct ProfileView: View, ProfileScreenInterface {
         
         VStack(spacing: 12) {
             Text("Subscribe to our newsletter")
+                .font(.appTitle)
             
             Text("Be the first to know about new collections and receive email-only discounts.")
-            
+                .font(.appBody)
+                .multilineTextAlignment(.center)
+        
             TextField("Email", text: $email)
+                .padding(.horizontal, 36)
+                .padding(.vertical, 14)
+                .overlay(Rectangle()
+                    .stroke(Color.black, lineWidth: 4))
+                .keyboardType(.emailAddress)
             
-            Button("Subscribe") {
-                // Action to subscribe to newsletter
+            Button {
+                //Action to subscribe to newsletter
+            } label: {
+                Text("Subscribe")
+                    .font(.appBody)
+                    .foregroundStyle(.black)
             }
-            
+            .padding(.horizontal, 36)
+            .padding(.vertical, 14)
+            .overlay(Rectangle()
+                .stroke(Color.black, lineWidth: 4))
+            .padding(.vertical, 8)
         }
         .padding(24)
         
         VStack(spacing: 4) {
             Link("Privacy Policy", destination: URL(string: "https://flashback.co.uk/policies/privacy-policy")!)
-                .font(.title)
-                .foregroundStyle(.red)
+                .font(.appBody)
+                .foregroundStyle(.black)
             Link("Returns Policy", destination: URL(string: "https://flashback.co.uk/pages/returns-policy")!)
-                .font(.title)
-                .foregroundStyle(.red)
+                .font(.appBody)
+                .foregroundStyle(.black)
             Link("Shipping Policy", destination: URL(string: "https://flashback.co.uk/pages/shipping-policy")!)
-                .font(.title)
-                .foregroundStyle(.red)
+                .font(.appBody)
+                .foregroundStyle(.black)
             Link("Contact Us", destination: URL(string: "https://flashback.co.uk/pages/contact-us")!)
-                .font(.title)
-                .foregroundStyle(.red)
+                .font(.appBody)
+                .foregroundStyle(.black)
             Link("About Us", destination: URL(string: "https://flashback.co.uk/pages/about-us")!)
-                .font(.title)
-                .foregroundStyle(.red)
+                .font(.appBody)
+                .foregroundStyle(.black)
         }
     }
 }
